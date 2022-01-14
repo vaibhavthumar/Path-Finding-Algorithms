861b0edf9ee83da635a9fc5bb75c127c0a83c57e
vaibhavthumar <43673056+vaibhavthumar@users.noreply.github.com>
2022-01-14 14:37:50 +1100

    DFS algorithm added

---
+++ DFS.py
@@ -0,0 +1,29 @@
+from queue import Queue
+
+def DFSAlgorithm(draw, construct_path, grid, start, end):
+    queue = Queue()
+    queue.put(start)
+    came_from = {}
+    visited = set()
+
+    while not queue.empty():
+        current = queue.get()
+        if current not in visited:
+            if current == end:
+                construct_path(came_from, end, draw)
+                end.make_end()
+                return True
+            else:
+                visited.add(current)
+                for neighbor in current.neighbors:
+                    if neighbor not in visited:
+                        queue.put(neighbor)
+                        came_from[neighbor] = current
+                        neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
