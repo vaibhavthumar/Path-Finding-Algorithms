05fe727390085d64babf6de26127a5f2221c2ead
vaibhavthumar <43673056+vaibhavthumar@users.noreply.github.com>
2022-01-14 13:54:16 +1100

    Button class modified
    button can be initialised with an image or passing button dimensions and colour

--- button.py
+++ button.py
@@ -1,15 +1,22 @@
 import pygame
 
 class Button():
-    def __init__(self, x, y, image, scale=1):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.rect = self.image.get_rect()
+    def __init__(self, x, y, width=100, height=30, color=pygame.Color('white'), scale=1, image=None):
+        if image:
+            self.image = pygame.transform.scale(image, (int(image.get_width() * scale), int(image.get_height() * scale)))
+            self.rect = self.image.get_rect()
+        else:
+            self.image = image
+            self.rect = pygame.Rect(x, y, width, height)
+            self.color = color
+
         self.rect.topleft = (x, y)
 
     def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+        if self.image:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+        else:
+            pygame.draw.rect(screen, self.color, self.rect)
 
     def is_clicked(self, pos):
         return self.rect.collidepoint(pos)
